9c149308ab7836d25e0febd408f0b9c85ae9c7c1
KoinoEngineering <email@koino.engineering>
2022-02-09 13:12:40 +0000

    add page template

---
+++ pages/hello-world/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bold">
+    ページのテンプレ
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator"
+
+@Component({})
+export default class KoinoSample extends Vue {}
+</script>
+<style lang="scss" scoped>
+// cssが必要な時のサンプル(要らない時は要らない)
+.bold {
+  font-weight: bold;
+}
+</style>
